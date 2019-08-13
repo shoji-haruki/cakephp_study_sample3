802a04941915952f0a070a04f892031fd67ce433
syoji-haruki <j_0@docomo.ne.jp>
2019-08-13 14:28:30 +0900

    アソシエーション、関連削除

--- config/routes.php
+++ config/routes.php
@@ -52,7 +52,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'admin', 'action' => 'top', 'home']);
+    $routes->connect('/', ['controller' => 'messages', 'action' => 'top', 'home']);
     $routes->connect('/admin', ['controller' => 'messages', 'action' => 'index', 'home']);
     // $routes->connect('/contact', ['controller' => 'messages', 'action' => 'add', 'home']);
 
