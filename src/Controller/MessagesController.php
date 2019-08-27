a899bfc5d6831b9ac1f41e9cea004f641a1d29f2
syoji-haruki <j_0@docomo.ne.jp>
2019-08-27 16:32:44 +0900

    自作api追加

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,8 +2,6 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
-// クライアント読み込み
-use Cake\Http\Client;
 
 /**
  * Messages Controller
@@ -54,19 +52,6 @@ class MessagesController extends AppController
 
     public function useapi(){
     }
-    // apiリクエストアクション
-    // public function api()
-    //     $http = new Client();
-    //     $response = $http->get('https://maps.googleapis.com/maps/api/js?key=AIzaSyAx9ceDdfuZPzme_j7SKVy0stkdpo5Qc44&callback=initMap ');
-    //     // var_dump($response);
-    //     // $this->set(compact( $response);
-    //     // if ($http->isOk) {
-    //     //     $json = json_decode($response->body());
-    //     // }
-    //     // var_dump($json);
-    // }
-    // public $uses = array('User');
-
 
     // 自作APIページ表示
     public function myapi(){
@@ -74,45 +59,13 @@ class MessagesController extends AppController
     // 自作api処理
     public function myapicustum()
     {
-        echo 'コントローラー通過';
         // 今回はJSONのみを返すためViewのレンダーを無効化
         $this->autoRender = false;
-        $result = $this->users->find()->where( username );
-        // var_dump($result);
-        return json_encode(compact('result'));
+        // テーブルからメッセージ全て取得
+        $data = $this->paginate($this->Messages);
+        // JSONに変換、bodyのみにして返す
+        $json_data = $this->response->body(json_encode($data));
     }
-         // Ajax以外の通信の場合
-        // if(!$this->request->is('ajax')) {
-        // throw new BadRequestException();
-        // }
-        /*  ここでDBアクセスなど何かの処理をする */
-        // $result = $this->$message->find('all');
-        // $result = $this->$message->find('all');
-        // 値が入っているかを確認。
-        // 値によっては(bool)でキャストしてしまうのも可
-
-        // var_dump($result);
-        // $status = !empty($result);
-        // if(!$status) {
-        // $error = array(
-        //     'message' => 'データがありません',
-        //     'code' => 404
-        // );
-        // }
-        // JSON形式で返却。errorが定義されていない場合はstatusとresultの配列になる。
-
-        // $this->autoRender = false;
-        // // レスポンスの形式をJSONで指定
-        // $this->response->type('application/json');
-        // // $http = new Client();
-        // $response = $http->get('https://qiita.com/');
-        // var_dump($response);
-        // if ($http->isOk) {
-        //     $json = json_decode($response->body());
-        // }
-        // var_dump($json);
-
-
 
     /**
      * Add method
@@ -127,7 +80,7 @@ class MessagesController extends AppController
             if ($this->Messages->save($message)) {
                 $this->Flash->success(__('The message has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['controller' => 'messages', 'action' => 'top', 'home']);
             }
             $this->Flash->error(__('The message could not be saved. Please, try again.'));
         }
--- webroot/js/apibtn.js
+++ webroot/js/apibtn.js
@@ -2,7 +2,6 @@ $(function(){
     $('#ajaxBtn').on('click', function(){
         $.ajax({
             type: 'get',
-            // dataType: 'html',
             url: '/messages/myapicustum',
         })
         .done( (data) => {
@@ -10,41 +9,8 @@ $(function(){
             $("#response").html(data);
             console.log(data);
         })
-        // Ajaxリクエストが失敗した時発動
         .fail( (data) => {
             alert('読み込み失敗');
         })
     });
 });
-        // .then(
-        //     data => console.log(data),
-        //     error => alert('読み込み失敗')
-        //     // // 1つめは通信成功時のコールバック
-        //     // function (data) {
-        //     //     // $("#results").append(data);
-        //     //     alert("成功");
-        //     // },
-        //     // // 2つめは通信失敗時のコールバック
-        //     // function () {
-        //     //     alert("読み込み失敗");
-        // );
-        // .then(
-        //     function(data) {
-        //     alert("成功");
-        //     console.log('aa');
-        //     var user = null;
-        //     // if(data.status) {
-        //         user = data.result;
-        //     function () {
-        //         alert("読み込み失敗");
-        //     });
-        //     // } else {
-        //     //     // 失敗時の処理。失敗したことを伝える。
-        //     //     alert(data.message);
-        //     // }
-        // });
-            // 'error': function() {
-            // // アクション側でExceptionが投げられた場合はここに来る。
-            // // エラーをここで処理したい場合はExceptionを投げても良い
-            // }
-
